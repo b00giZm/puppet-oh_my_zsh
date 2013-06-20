c44114d6ae49dfbd71024b427e1b6811768ab72e
b00gizm <b00gizm@gmail.com>
2013-06-20 16:43:16 +0200

    Install oh-my-zsh via git

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,29 @@
-# Public: Installs oh-my-zsh via the automatic installer script.
+# Public: Installs oh-my-zsh via using git.
 #
 # Examples
 #
 #   include oh-my-zsh
+
 class oh_my_zsh {
-  exec { 'curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | /bin/sh':
-    path => '/usr/bin',
+  include zsh
+
+  $user_home = "/Users/${::luser}"
+  $dest_dir  = "${user_home}/.oh-my-zsh"
+  
+  repository { $dest_dir:
+    source  => 'robbyrussell/oh-my-zsh',
+    provider => 'git',
   }
+  
+  exec { 'backup_zshrc':
+    command   => "cat ${user_home}/.zshrc > ${user_home}/.zshrc.pre-oh-my-zsh",
+    onlyif    => "test -f ${user_home}/.zshrc",
+    subscribe => Repository[$dest_dir],
+  }
+  
+  exec { 'copy_zshrc_config_template':
+    command   => "cp ${dest_dir}/templates/zshrc.zsh-template ${user_home}/.zshrc",
+    subscribe => Exec['backup_zshrc']
+  }
+
 }
